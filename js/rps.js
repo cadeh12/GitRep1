fa5a92c1ebd48b499b40e8464b476ae2af2526ab
cadeh12 <chemond1012@gmail.com>
2023-11-02 12:43:06 -0400

    commit

--- js/rps.js
+++ js/rps.js
@@ -1,3 +1,4 @@
+// the DOM (Document Object Model)
 const rockBtn = document.getElementById('rock')
 const paperBtn = document.getElementById('paper')
 const scissorsBtn = document.getElementById('scissors')
@@ -5,16 +6,43 @@ const result = document.querySelector('.result')
 const userImg = document.querySelector('.user-img')
 const compImg = document.querySelector('.comp-img')
 
-function rockBtnClick() {
-    result.innerText = 'rock clicked'
-}
-function paperBtnClick() {
-    result.innerText = 'paper clicked'
-}
-function scissorsBtnClick() {
-    result.innerText = 'scissors clicked'
+const choices = ['rock', 'paper', 'scissors']
+let comp
+
+// function rockBtnClick() {
+    //to change the text of the result object
+//     result.innerText = 'rock clicked'
+// }
+// function paperBtnClick() {
+//     result.innerText = 'paper clicked'
+// }
+// function scissorsBtnClick() {
+//     result.innerText = 'scissors clicked'
+// }
+
+rockBtn.addEventListener('click', () => {
+    userImg.src = "../img/rps_img/" + rockBtn.id + ".png"
+    // result.style.backgroundColor = "red"
+    compChoice()
+    winLose(rockBtn.id)
+})
+paperBtn.addEventListener('click', () => {
+    userImg.src = "../img/rps_img/" + paperBtn.id + ".png"
+    compChoice()
+    winLose(paperBtn.id)
+})
+scissorsBtn.addEventListener('click', () => {
+    userImg.src = "../img/rps_img/" + scissorsBtn.id +".png"
+    compChoice()
+    winLose(scissorsBtn.id)
+})
+
+function compChoice() {
+    const randNum = Math.floor(Math.random()*3)
+    comp = choices[randNum]
+    compImg.src = "../img/rps_img/" + comp +".png"
 }
 
-rockBtn.addEventListener('click', rockBtnClick)
-paperBtn.addEventListener('click', paperBtnClick)
-scissorsBtn.addEventListener('click', scissorsBtnClick)
+function winLose(userChoice) {
+    console.log(comp, userChoice)
+}
