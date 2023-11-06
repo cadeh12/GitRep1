e357c3b913b8ae8609a6ef28319de10b1ade9f59
cadeh12 <chemond1012@gmail.com>
2023-11-06 12:45:06 -0500

    commit

--- js/rps.js
+++ js/rps.js
@@ -5,9 +5,17 @@ const scissorsBtn = document.getElementById('scissors')
 const result = document.querySelector('.result')
 const userImg = document.querySelector('.user-img')
 const compImg = document.querySelector('.comp-img')
+const userScore = document.querySelector('.userScore')
+const compScore = document.querySelector('.compScore')
+const resetBtn = document.getElementById('reset')
+const overall = document.querySelector('.overall')
+
+let userCount = 0
+let compCount = 0
 
 const choices = ['rock', 'paper', 'scissors']
 let comp
+let userChoice
 
 // function rockBtnClick() {
     //to change the text of the result object
@@ -36,6 +44,9 @@ scissorsBtn.addEventListener('click', () => {
     compChoice()
     winLose(scissorsBtn.id)
 })
+resetBtn.addEventListener('click', () => {
+    window.location.reload()
+})
 
 function compChoice() {
     const randNum = Math.floor(Math.random()*3)
@@ -44,5 +55,38 @@ function compChoice() {
 }
 
 function winLose(userChoice) {
-    console.log(comp, userChoice)
+    if (userChoice == "rock" && comp == "paper") {
+        result.innerText = "Computer Wins"
+        compCount = compCount + 1
+        compScore.innerText = compCount.toString()
+    } else if (userChoice == "paper" && comp == "scissors") {
+        result.innerText = "Computer Wins"
+        compCount = compCount + 1
+        compScore.innerText = compCount.toString()
+    } else if (userChoice == "scissors" && comp == "rock") {
+        result.innerText = "Computer Wins"
+        compCount = compCount + 1
+        compScore.innerText = compCount.toString()
+    } else if (userChoice == "rock" && comp == "scissors") {
+        result.innerText = "User Wins"
+        userCount = userCount + 1
+        userScore.innerText = userCount.toString()
+    } else if (userChoice == "paper" && comp == "rock") {
+        result.innerText = "User Wins"
+        userCount = userCount + 1
+        userScore.innerText = userCount.toString()
+    } else if (userChoice == "scissors" && comp == "paper") {
+        result.innerText = "User Wins"
+        userCount = userCount + 1
+        userScore.innerText = userCount.toString()
+    } else {
+        result.innerText = "Tie"
+    }
+}
+function bestOf5 () {
+    if (userScore == "5") {
+        overall.innerText = "YOU WIN"
+    } else if (compScore = "5") {
+        overall.innerText = "YOU LOSE"
+    }
 }
