03ebc6415cf106ff9021a6e6e47189bd45f43055
cadeh12 <chemond1012@gmail.com>
2024-03-08 12:15:53 -0500

    commit

--- js/indexHamburger.js
+++ js/indexHamburger.js
@@ -3,6 +3,8 @@ const menuItems = document.querySelectorAll('.link')
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.getElementById('closeIcon');
 const menuIcon = document.getElementById('menuIcon');
+const theJoke = document.querySelector(".output")
+const jokeBtn = document.querySelector(".button")
 
 function toggleMenu() {
     if (menu.classList.contains("displayMenu")) {
@@ -17,3 +19,28 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener("click", toggleMenu)
+
+const url = 'https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd245082211msh9526fbad8f13081p1c9307jsnebb6e983526f',
+    'X-RapidAPI-Host': 'dad-jokes-by-api-ninjas.p.rapidapi.com'
+  }
+};
+
+
+
+async function getJoke() {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    const joke = result[0].joke;
+    console.log(result[0].joke);
+    theJoke.textContent = joke;
+  } catch (error) {
+    theJoke.textContent = "something went awry..."
+  }
+}
+
+jokeBtn.addEventListener("click", getJoke)
