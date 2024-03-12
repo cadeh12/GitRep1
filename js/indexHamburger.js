3153d3a2fd7a85baf546a93a183d33cad408e91c
cadeh12 <chemond1012@gmail.com>
2024-03-12 12:07:51 -0400

    commit

--- js/indexHamburger.js
+++ js/indexHamburger.js
@@ -18,6 +18,11 @@ function toggleMenu() {
     }
 }
 
+https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={9f9b4e7d7ecee25c8e5c3529ea6d8b76}
+
+
+
+
 hamburger.addEventListener("click", toggleMenu)
 
 const url = 'https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes';
