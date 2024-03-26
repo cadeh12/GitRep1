b311165605e103ebe122e8d108c2b0b344446c50
cadeh12 <chemond1012@gmail.com>
2024-03-26 12:29:12 -0400

    commit

--- js/index.js
+++ js/index.js
@@ -52,42 +52,80 @@ const conditionIcon = document.querySelector(".con-img")
 const currentCondition = document.querySelector(".condition")
 const currentTemp = document.querySelector(".current-temp")
 
-weatherCity.textContent = "PORTLAND, ME"
-// Date and Time
-const dateObject = new Date();
-const month = dateObject.getMonth() + 1
-const day = dateObject.getDate()
-const year = dateObject.getFullYear()
-weatherDate.textContent = month + "/" + day + "/" + year
-const hours = dateObject.getHours()
-let minutes = dateObject.getMinutes()
-if (minutes < 10) {
-  minutes = "0" + dateObject.getMinutes()
+//Geolocation API
+const geoKey = "fe5ae5c5c87f42c3a73839eda03a4fb0"
+
+async function getLocation() {
+  const urlGeo = `https://api.geoapify.com/v1/ipinfo?&apiKey=${geoKey}`
+  try {
+    const response = await fetch(urlGeo)
+    const data = await response.json()
+    const city = data.city.name
+    const state = data.state.name
+    weatherCity.textContent = city + ", " + state
+    const lat = data.location.latitude
+    const lon = data.location.longitude
+    const coords = [lat, lon]
+    return coords
+  } catch (error) {
+    console.error(error)
+  }
 }
-let meridian = "AM"
-if (hours > 12) {
-  hours = dateObject.getHours() - 12
+
+getLocation().then(c => getTime(c))
+getLocation().then(c => getWeather(c))
+
+//Time API
+async function getTime(coords) {
+  const lat = coords[0]
+  const lon = coords[1]
+  const url = `https://api.api-ninjas.com/v1/worldtime?lat=${lat}&lon=${lon}`
+  const options = {
+      method: 'GET',
+      headers: {'X-API-Key': 'Ct4PxboL3SwP5CSpmebI7A==nodU4TvQTzcxUuER'}
+    }
+  try {
+    const response = await fetch(url, options)
+    const data = await response.json()
+    const month = data.month
+    const day = data.day
+    const year = data.year
+    weatherDate.textContent = month + "/" + day + "/" + year
+    var hours = data.hour
+    let minutes = data.minute
+    if (minutes < 10) {
+      minutes = "0" + data.minute
+    }
+    let meridian = "PM"
+    if (hours < 12) {
+      meridian = "AM"
+    }
+    if (hours > 12) {
+      hours = data.hour - 12
+    }
+    let time = `${hours}:${minutes} ${meridian}`
+    weatherTime.textContent = time
+    // Update Copyright year
+    const currentYear = document.querySelector(".current-year")
+    currentYear.textContent = year
+  } catch (error) {
+    console.error(error)
+  }
 }
-if (hours >= 12) {
-  meridian = "PM"
-}
-let time = `${hours}:${minutes} ${meridian}`
-weatherTime.textContent = time
-// Update Copyright year
-const currentYear = document.querySelector(".current-year")
-currentYear.textContent = year
+
+getTime()
 
 // Weather API
 const clouds = ['few clouds', 'scattered clouds', 'broken clouds', 'overcast clouds']
-const rain = ['shower rain', 'rain', 'mist']
-let lat = 43.6591
-let lon = -70.2568
+const rain = ['shower rain', 'rain', 'mist', 'light rain', 'moderate rain']
 const key = "9f9b4e7d7ecee25c8e5c3529ea6d8b76"
-let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
 let img
 
-async function getWeather() {
+async function getWeather(coords) {
+  const lat = coords[0]
+  const lon = coords[1]
   try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`
     const response = await fetch(url)
     const data = await response.json()
     // console.log(data.weather[0].main)
